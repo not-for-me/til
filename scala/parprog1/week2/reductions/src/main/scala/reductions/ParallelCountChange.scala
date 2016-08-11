f2b22005fe18704e3238f62e8465d3b9896a0d63
조우진 <woojin@tmon.co.kr>
2016-08-11 19:05:02 +0900

    study parprog1 week2

--- scala/parprog1/week2/reductions/src/main/scala/reductions/ParallelCountChange.scala
+++ scala/parprog1/week2/reductions/src/main/scala/reductions/ParallelCountChange.scala
@@ -1,7 +1,7 @@
 package reductions
 
-import org.scalameter._
 import common._
+import org.scalameter._
 
 object ParallelCountChangeRunner {
 
@@ -14,7 +14,7 @@ object ParallelCountChangeRunner {
     Key.exec.maxWarmupRuns -> 40,
     Key.exec.benchRuns -> 80,
     Key.verbose -> true
-  ) withWarmer(new Warmer.Default)
+  ) withWarmer (new Warmer.Default)
 
   def main(args: Array[String]): Unit = {
     val amount = 250
@@ -43,32 +43,49 @@ object ParallelCountChangeRunner {
 object ParallelCountChange {
 
   /** Returns the number of ways change can be made from the specified list of
-   *  coins for the specified amount of money.
-   */
+    * coins for the specified amount of money.
+    */
   def countChange(money: Int, coins: List[Int]): Int = {
-    ???
+    val sortedCoins = coins.sortBy(-_)
+
+    def _loop(money: Int, coins: List[Int]): Integer = {
+      if (money < 0) 0
+      else if (money == 0) 1
+      else if (coins.isEmpty) 0
+      else _loop(money, coins.tail) + _loop(money - coins.head, coins)
+    }
+
+    _loop(money, sortedCoins)
   }
 
   type Threshold = (Int, List[Int]) => Boolean
 
   /** In parallel, counts the number of ways change can be made from the
-   *  specified list of coins for the specified amount of money.
-   */
+    * specified list of coins for the specified amount of money.
+    */
   def parCountChange(money: Int, coins: List[Int], threshold: Threshold): Int = {
-    ???
+    if (threshold(money, coins)) countChange(money, coins)
+    else {
+      if (coins.isEmpty) 0
+      else {
+        val result = parallel(parCountChange(money, coins.tail, threshold), parCountChange(money - coins.head, coins, threshold))
+        result._1 + result._2
+      }
+    }
+
   }
 
   /** Threshold heuristic based on the starting money. */
-  def moneyThreshold(startingMoney: Int): Threshold =
-    ???
+  def moneyThreshold(startingMoney: Int): Threshold = (money, coins) => (startingMoney * 2) / 3 >= money
 
   /** Threshold heuristic based on the total number of initial coins. */
-  def totalCoinsThreshold(totalCoins: Int): Threshold =
-    ???
-
+  def totalCoinsThreshold(totalCoins: Int): Threshold = (money, coins) => (totalCoins * 2) / 3 >= coins.length || money < 1
 
   /** Threshold heuristic based on the starting money and the initial list of coins. */
-  def combinedThreshold(startingMoney: Int, allCoins: List[Int]): Threshold = {
-    ???
+  def combinedThreshold(startingMoney: Int, allCoins: List[Int]): Threshold = (money, coins) => {
+    val thresholdNumber = startingMoney * allCoins.length * 2
+    val curNumber = money * coins.length
+
+    thresholdNumber >= curNumber
   }
 }
--- scala/parprog1/week2/reductions/src/main/scala/reductions/ParallelParenthesesBalancing.scala
+++ scala/parprog1/week2/reductions/src/main/scala/reductions/ParallelParenthesesBalancing.scala
@@ -39,24 +39,46 @@ object ParallelParenthesesBalancingRunner {
 object ParallelParenthesesBalancing {
 
   /** Returns `true` iff the parentheses in the input `chars` are balanced.
-   */
+    */
   def balance(chars: Array[Char]): Boolean = {
-    ???
+    def loop(acc: Int, chars: List[Char]): Boolean = chars match {
+      case x :: xs if acc < 0 => false
+      case x :: xs if x == '(' => loop(acc + 1, xs)
+      case x :: xs if x == ')' => loop(acc - 1, xs)
+      case x :: xs => loop(acc, xs)
+      case _ => acc == 0
+    }
+    loop(0, chars.toList)
   }
 
   /** Returns `true` iff the parentheses in the input `chars` are balanced.
-   */
+    */
   def parBalance(chars: Array[Char], threshold: Int): Boolean = {
 
-    def traverse(idx: Int, until: Int, arg1: Int, arg2: Int) /*: ???*/ = {
-      ???
+    def traverse(idx: Int, until: Int, arg1: Int, arg2: Int): (Int, Int) = {
+      def loop(chars: List[Char], arg1: Int, arg2: Int): (Int, Int) = chars match {
+        case x :: xs if arg2 > arg1 => (0, -1)
+        case x :: xs if x == '(' => loop(xs, arg1 + 1, arg2)
+        case x :: xs if x == ')' => loop(xs, arg1, arg2 + 1)
+        case x :: xs => loop(xs, arg1, arg2)
+        case _ => (arg1, arg2)
+      }
+
+      loop(chars.slice(idx, until).toList, 0, 0)
     }
 
-    def reduce(from: Int, until: Int) /*: ???*/ = {
-      ???
+    def reduce(from: Int, until: Int): Int = {
+      if (chars.length < threshold) {
+        val result = traverse(from, until, 0, 0)
+        result._1 - result._2
+      } else {
+        val mid = (until - from) / 2
+        val result = parallel(reduce(from, mid), reduce(mid, until))
+        result._1 + result._2
+      }
     }
 
-    reduce(0, chars.length) == ???
+    reduce(0, chars.length) == 0
   }
 
   // For those who want more:
